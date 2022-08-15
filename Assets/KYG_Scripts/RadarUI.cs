e7315af1bd39bc557ab62c9b000bef2e2d15eaac
Ellian <62099455+ygsh0315@users.noreply.github.com>
2022-08-16 02:30:50 +0900

    Radar

---
+++ Assets/KYG_Scripts/RadarUI.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class RadarUI : MonoBehaviour
+{
+    public static RadarUI Instance;
+    public GameObject Player;
+    public Image PlayerLocation;
+    public Image EnemyLocation;
+    public List<GameObject> DetectedEnemyList = new List<GameObject>();
+    public List<Image> EnemyLocationGroup = new List<Image>();
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerLocation.transform.rotation = Quaternion.Euler(0, 0, -Player.transform.rotation.eulerAngles.y);
+        foreach(GameObject Enemy in DetectedEnemyList)
+        {
+            if (!Enemy.gameObject)
+            {
+                Destroy(EnemyLocationGroup[DetectedEnemyList.IndexOf(Enemy)]);
+                EnemyLocationGroup.RemoveAt(DetectedEnemyList.IndexOf(Enemy));
+                DetectedEnemyList.RemoveAt(DetectedEnemyList.IndexOf(Enemy));
+            }
+        }
+        if (EnemyLocationGroup.Count < DetectedEnemyList.Count)
+        {
+            Image enemyLocation = Instantiate(EnemyLocation);
+            EnemyLocationGroup.Add(enemyLocation);
+            enemyLocation.transform.SetParent(this.transform.GetChild(0));            
+        }
+        for(int i = 0; i<EnemyLocationGroup.Count; i++)
+        {
+            EnemyLocationGroup[i].transform.position =PlayerLocation.transform.position + new Vector3((Player.transform.position- DetectedEnemyList[i].transform.position ).x * -0.05f, (DetectedEnemyList[i].transform.position - Player.transform.position).z * 0.05f, 0);
+        }
+    }
+}
