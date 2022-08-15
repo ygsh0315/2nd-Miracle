2723d50ce710839228c0c142d88e5cbb770bb410
Ellian <62099455+ygsh0315@users.noreply.github.com>
2022-08-15 22:18:20 +0900

    lader

--- Assets/KYG_Scripts/Lader.cs
+++ Assets/KYG_Scripts/Lader.cs
@@ -1,12 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+
 public class Lader : MonoBehaviour
 {
-    public Image PlayerLocation;
-    public GameObject Player;
-    public List<Image> EnemyLocationGroup = new List<Image>();
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +13,6 @@ public class Lader : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        PlayerLocation.transform.rotation = Quaternion.Euler(0, 0, -Player.transform.rotation.eulerAngles.y);
-
         
     }
 }
